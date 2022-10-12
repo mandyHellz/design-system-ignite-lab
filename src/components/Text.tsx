ae9f0dbf68fac0dd3f99b2a110ce96a8933c3be8
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-10-11 23:10:37 -0300

    feat: created Text component

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,7 +1,24 @@
-export const Text = () => {
+import { clsx } from "clsx";
+import { Slot } from "@radix-ui/react-slot";
+import { ReactNode } from "react";
+export interface TextProps {
+  size?: "sm" | "md" | "lg";
+  children: ReactNode;
+  asChild?: boolean;
+}
+
+export const Text = ({ size = "md", children, asChild }: TextProps) => {
+  const Comp = asChild ? Slot : "span";
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Comp
+      className={clsx("text-gray-100 font-sans", {
+        "text-xs": size === "sm",
+        "text-sm": size === "md",
+        "text-md": size === "lg",
+      })}
+    >
+      {children}
+    </Comp>
   );
 };
