5bb33027c9c2f00a424f02192b8c51d8bd72e486
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-10-11 23:11:31 -0300

    docs: add Text component

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,36 @@
+import { Text, TextProps } from "./Text";
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "Components/Text",
+  component: Text,
+  args: {
+    children: "Text example",
+  },
+} as Meta<TextProps>;
+
+export const Default: StoryObj<TextProps> = {};
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: "sm",
+  },
+};
+
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: "lg",
+  },
+};
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: (
+      <div>
+        <p>Component is no more a span.</p>
+        <p>Now its a div with 2 children paragraphs as example.</p>
+      </div>
+    ),
+  },
+};
