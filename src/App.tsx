914154bb9e33255d5b515f71d32293e8a5305c70
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-10-13 22:17:59 -0300

    fix: removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { Envelope, Lock } from "phosphor-react";
-import { Button } from "./components/Button";
-import { Checkbox } from "./components/Checkbox";
-import { SignIn } from "./pages/Signin";
+import { Signin } from "./pages/Signin";
+
 import "./styles/global.css";
 
 export function App() {
   return (
     <div>
-      <SignIn />
+      <Signin />
     </div>
   );
 }
